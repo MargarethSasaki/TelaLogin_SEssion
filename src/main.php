500cf8865a5dad5cdebd447113670d06e30026cb
Margareth Sasaki <magasasaki@gmail.com>
2023-05-08 20:42:21 -0300

    first commit

---
+++ src/main.php
@@ -0,0 +1,25 @@
+<?php
+
+include_once(__DIR__."/../vendor/autoload.php");
+
+use App\model\Usuario;
+
+if (!isset($_SESSION['uLogado'])) {
+    $_SESSION['uLogado'] = "";
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if ( $_POST['nome'] == 'admin' && $_POST['pass'] == 'admin' ){
+        $uAdminPadrao = new Usuario();
+        $uAdminPadrao -> setName('admin');
+        $uAdminPadrao -> setPass('admin');
+        $_SESSION['uLogado'] = $uAdminPadrao -> getName();
+        echo 'Login realizado com sucesso';
+    }
+    else{
+
+        echo "Falha no Login";
+    } 
+
+
+}
